21998850edb56db11967603d7f096819b557f7f6
Yurii Pakhota <Ov3RforDream@gmail.com>
2022-04-07 15:13:45 +0300

    Utils tests fix

--- server/src/lib/utils.ts
+++ server/src/lib/utils.ts
@@ -1,19 +1,27 @@
-import fetch, { HeadersInit, BodyInit, FetchError } from 'node-fetch';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import axios, { AxiosRequestConfig, AxiosRequestHeaders } from 'axios';
 import { IUser } from '../interfaces/dto/user.dto';
 import { IFakeUser } from '../interfaces/fake-user.interface';
+import { APIError, ErrorCode } from '../services/api-error.service';
 
 export const fetchAPI = async ({ url, method = 'GET', headers, body }: {
-    url?: string,
-    method?: string,
-    headers?: HeadersInit,
-    body?: BodyInit
-}) => fetch(url, { method, headers, body })
-    .then(res => res.json())
+    url: string,
+    method?: 'GET' | 'POST',
+    headers?: AxiosRequestHeaders,
+    body?: AxiosRequestConfig
+}) => axios(url, { method, headers, data: body })
+    .then(res => {
+        if (res.status >= 400) {
+            throw new APIError(ErrorCode.BAD_REQUEST);
+        }
+        return res.data;
+    })
     .catch(err => console.error(err));
 
 export const createFakeUsers = async (num: number) => {
     const url = `https://randomuser.me/api/?results=${num}`;
-    const { results }: Record<string, any> = await fetchAPI({ url });
+    const { results }: { results: Record<string, any> } = await fetchAPI({ url });
 
     const users = results.map((user: Record<string, any> & IFakeUser, index: number) => ({
         id: index,
@@ -30,3 +38,4 @@ export const createFakeUsers = async (num: number) => {
 
     return users;
 };
+/* eslint-disable @typescript-eslint/no-explicit-any */
--- server/src/tests/utils.test.ts
+++ server/src/tests/utils.test.ts
@@ -1,165 +1,153 @@
-// jest.mock('node-fetch');
-// console.log('fetch-mock', require('fetch-mock-jest'))
-jest.mock('node-fetch', () => jest.fn());
-const nodeFetch = require('node-fetch');
-const { fetchAPI } = require('../lib/utils');
-// import * as fetchModule from 'node-fetch';
-// const fetchModule = require('node-fetch');
-// const fetch = fetchModule.default;
-// console.log('fetch', fetch);
+/* eslint-disable */
 
+import { IUser } from '../interfaces/dto/user.dto';
 
-// import { IUser } from '../interfaces/dto/user.dto';
+jest.mock('axios');
 
-// jest.mock('../lib/utils', () => ({
-//     __esModule: true,
-//     fetchAPI: jest.fn()
-// }));
+const axios = require('axios') as jest.Mock;
+const { fetchAPI, createFakeUsers } = require('../lib/utils');
 
-// ------- NOT WORKING
+describe('Utils test', () => {
 
-describe('Utils Test', () => {
+    it('Fetch API method (axios)', async () => {
+        axios.mockResolvedValue({ data: 5 });
 
-    it('', async () => {
-        // MOCK FETCH HERE
-        nodeFetch.mockReturnValue(Promise.resolve(5));
+        const res = await fetchAPI({});
 
-        // CALL fetchAPI()
-        const response = await fetchAPI({});
-
-        // EXPECT TO BE WORKING
-        expect(nodeFetch).toHaveBeenCalled();
-        expect(response).toBe(5);
-
-        // console.log(utils);
+        expect(axios).toHaveBeenCalled();
+        expect(res).toBe(5);
     });
 
-    // it('Should create 2 fake users', async () => {
-        // const fakeUsers: Record<string, any> = {
-        //     results: [
-        //         {
-        //             gender: 'female',
-        //             id: {
-        //                 name: '12312',
-        //                 value: '12312'
-        //             },
-        //             name: {
-        //                 title: 'Ms',
-        //                 first: 'Allie',
-        //                 last: 'Spencer'
-        //             },
-        //             login: {
-        //                 username: 'Test_1'
-        //             },
-        //             registered: {
-        //                 date: '2018-08-01T01:31:27.842Z',
-        //                 age: 4
-        //             },
-        //             dob: {
-        //                 date: '1969-04-16T16:21:44.438Z',
-        //                 age: 53
-        //             },
-        //             location: {
-        //                 country: 'Country 1',
-        //                 city: 'City 1'
-        //             },
-        //             picture: {
-        //                 large: 'https://randomuser.me/api/portraits/women/92.jpg',
-        //                 medium: 'https://randomuser.me/api/portraits/med/women/92.jpg',
-        //                 thumbnail: 'https://randomuser.me/api/portraits/thumb/women/92.jpg'
-        //             }
-        //         },
-        //         {
-        //             gender: 'female',
-        //             name: {
-        //                 title: 'Mrs',
-        //                 first: 'Diana',
-        //                 last: 'Richards'
-        //             },
-        //             location: {
-        //                 street: {
-        //                     number: 4590,
-        //                     name: 'Hickory Creek Dr'
-        //                 },
-        //                 city: 'Modesto',
-        //                 state: 'Colorado',
-        //                 country: 'United States',
-        //                 postcode: 55951,
-        //                 coordinates: {
-        //                     latitude: '45.9148',
-        //                     longitude: '-72.8817'
-        //                 },
-        //                 timezone: {
-        //                     offset: '-2:00',
-        //                     description: 'Mid-Atlantic'
-        //                 }
-        //             },
-        //             email: 'diana.richards@example.com',
-        //             login: {
-        //                 uuid: '3f7410ad-0102-44f9-8c86-75918da28fdc',
-        //                 username: 'smallgorilla314',
-        //                 password: 'puffy',
-        //                 salt: 'ZOiG83WP',
-        //                 md5: 'c266b709e8c983ff49f0e63017953500',
-        //                 sha1: 'bfe1872e7558ee9b4210f0ed522b4f43524e6d87',
-        //                 sha256: 'b7dd4024281c87fa55efec0d6bb299807749c752d713a60d7c2c3dba43b3035f'
-        //             },
-        //             dob: {
-        //                 date: '1948-08-06T10:01:25.729Z',
-        //                 age: 74
-        //             },
-        //             registered: {
-        //                 date: '2018-04-23T23:09:18.528Z',
-        //                 age: 4
-        //             },
-        //             phone: '(515)-569-4911',
-        //             cell: '(538)-084-7708',
-        //             id: {
-        //                 name: 'SSN',
-        //                 value: '055-90-7979'
-        //             },
-        //             picture: {
-        //                 large: 'https://randomuser.me/api/portraits/women/23.jpg',
-        //                 medium: 'https://randomuser.me/api/portraits/med/women/23.jpg',
-        //                 thumbnail: 'https://randomuser.me/api/portraits/thumb/women/23.jpg'
-        //             },
-        //             nat: 'US'
-        //         },
-        //     ]
-        // };
-        // const fetch = await import('node-fetch');
-        // const { fetchAPI } = await import('../lib/utils');
-        // (fetchAPI as jest.Mock).mockReturnValue(Promise.resolve(fakeUsers));
+    it('Should return 2 fake users', async () => {
+        const fakeUsers: Record<string, any> = {
+            results: [
+                {
+                    gender: 'female',
+                    id: {
+                        name: '12312',
+                        value: '12312'
+                    },
+                    name: {
+                        title: 'Ms',
+                        first: 'Allie',
+                        last: 'Spencer'
+                    },
+                    login: {
+                        username: 'Test_1'
+                    },
+                    registered: {
+                        date: '2018-08-01T01:31:27.842Z',
+                        age: 4
+                    },
+                    dob: {
+                        date: '1969-04-16T16:21:44.438Z',
+                        age: 53
+                    },
+                    location: {
+                        country: 'Country 1',
+                        city: 'City 1'
+                    },
+                    picture: {
+                        large: 'https://randomuser.me/api/portraits/women/92.jpg',
+                        medium: 'https://randomuser.me/api/portraits/med/women/92.jpg',
+                        thumbnail: 'https://randomuser.me/api/portraits/thumb/women/92.jpg'
+                    }
+                },
+                {
+                    gender: 'female',
+                    name: {
+                        title: 'Mrs',
+                        first: 'Diana',
+                        last: 'Richards'
+                    },
+                    location: {
+                        street: {
+                            number: 4590,
+                            name: 'Hickory Creek Dr'
+                        },
+                        city: 'Modesto',
+                        state: 'Colorado',
+                        country: 'United States',
+                        postcode: 55951,
+                        coordinates: {
+                            latitude: '45.9148',
+                            longitude: '-72.8817'
+                        },
+                        timezone: {
+                            offset: '-2:00',
+                            description: 'Mid-Atlantic'
+                        }
+                    },
+                    email: 'diana.richards@example.com',
+                    login: {
+                        uuid: '3f7410ad-0102-44f9-8c86-75918da28fdc',
+                        username: 'smallgorilla314',
+                        password: 'puffy',
+                        salt: 'ZOiG83WP',
+                        md5: 'c266b709e8c983ff49f0e63017953500',
+                        sha1: 'bfe1872e7558ee9b4210f0ed522b4f43524e6d87',
+                        sha256: 'b7dd4024281c87fa55efec0d6bb299807749c752d713a60d7c2c3dba43b3035f'
+                    },
+                    dob: {
+                        date: '1948-08-06T10:01:25.729Z',
+                        age: 74
+                    },
+                    registered: {
+                        date: '2018-04-23T23:09:18.528Z',
+                        age: 4
+                    },
+                    phone: '(515)-569-4911',
+                    cell: '(538)-084-7708',
+                    id: {
+                        name: 'SSN',
+                        value: '055-90-7979'
+                    },
+                    picture: {
+                        large: 'https://randomuser.me/api/portraits/women/23.jpg',
+                        medium: 'https://randomuser.me/api/portraits/med/women/23.jpg',
+                        thumbnail: 'https://randomuser.me/api/portraits/thumb/women/23.jpg'
+                    },
+                    nat: 'US'
+                },
+            ]
+        };
+        axios.mockResolvedValue({ data: fakeUsers });
 
-        // const response = await createFakeUsers(2);
+        const users: IUser[] = await createFakeUsers(2);
 
-        // expect(fetch).toHaveBeenCalled();
-        // expect(fetch).toHaveBeenCalledWith({ url: 'https://randomuser.me/api/?results=2' });
-        // expect(response).toEqual([
-        //     {
-        //         id: 1,
-        //         name: 'Allie Spencer',
-        //         username: 'Test_1',
-        //         problems: 4,
-        //         solves: 53,
-        //         location: {
-        //             country: 'Country 1',
-        //             city: 'City 1'
-        //         },
-        //         avatar: 'https://randomuser.me/api/portraits/women/92.jpg'
-        //     },
-        //     {
-        //         id: 2,
-        //         name: 'Diana Richards',
-        //         username: 'smallgorilla314',
-        //         problems: 4,
-        //         solves: 74,
-        //         location: {
-        //             country: 'United States',
-        //             city: 'Modesto'
-        //         },
-        //         avatar: 'https://randomuser.me/api/portraits/women/23.jpg'
-        //     }
-        // ] as IUser[]);
-    // });
+        expect(axios).toHaveBeenCalled();
+        expect(axios).toHaveBeenLastCalledWith('https://randomuser.me/api/?results=2', {
+            method: 'GET',
+            headers: undefined,
+            data: undefined
+        });
+        expect(users).toEqual([
+            {
+                id: 0,
+                name: 'Allie Spencer',
+                username: 'Test_1',
+                problems: 4,
+                solves: 53,
+                location: {
+                    country: 'Country 1',
+                    city: 'City 1'
+                },
+                avatar: 'https://randomuser.me/api/portraits/women/92.jpg'
+            },
+            {
+                id: 1,
+                name: 'Diana Richards',
+                username: 'smallgorilla314',
+                problems: 4,
+                solves: 74,
+                location: {
+                    country: 'United States',
+                    city: 'Modesto'
+                },
+                avatar: 'https://randomuser.me/api/portraits/women/23.jpg'
+            }
+        ] as IUser[]);
+    });
 });
+
+/* eslint-disable */
